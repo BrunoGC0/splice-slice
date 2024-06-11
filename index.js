c7cf056a6e1e0ddcf79ffe19665ff87417593d61
Bruno Gonçalves <brunogcp28@gmail.com>
2024-06-11 08:17:00 -0400

    Aula 09 JS intermediário - Splice e slice

---
+++ index.js
@@ -0,0 +1,14 @@
+let frutas = ['Banana', 'Laranja', 'Morango', 'Pêra']
+
+// let frutasExtraidas = frutas.slice(1)
+
+// console.log(frutasExtraidas)
+
+// let frutasExtraidas = frutas.splice(0,2)
+
+// console.log(frutasExtraidas)
+
+let removeEAdcionaFrutas = frutas.splice(1,2, 'Goiaba' , 'Abacaxi', 'Maçã')
+
+console.log(frutas)
+
